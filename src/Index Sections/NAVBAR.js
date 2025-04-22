a6509c50b298eb68adda8d116e58fc3b2d3c1eae
Pldkoit0493 <geraldmariscotes@gmail.com>
2025-04-23 07:09:31 +0800

    Section Components, Logo update

---
+++ src/Index Sections/NAVBAR.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+export default function NAVBAR() {
+  return (
+    <>
+      <header className="top_bar">
+        <div className="cafe_logo">
+          <img src={require('../Assets/cafelogo.png')} alt="GV CAFE" />
+          <h1>GV CAFE</h1>
+        </div>
+        <button className="menu-toggle" aria-label="Open Menu">
+          <lord-icon
+            src="https://cdn.lordicon.com/eouimtlu.json"
+            trigger="hover"
+            colors="primary:#000000"
+            style={{ width: 30, height: 30 }}
+          ></lord-icon>
+        </button>
+        <div className="navbar">
+          <nav>
+            <ul className="links">
+              <li>
+                <a href="#HomePage" className="box-bar">
+                  HOME
+                </a>
+              </li>
+              <li>
+                <a href="#ProductSelection" className="box-bar">
+                  PRODUCT
+                </a>
+              </li>
+              <li>
+                <a href="#aboutus" className="box-bar">
+                  ABOUT US
+                </a>
+              </li>
+              <li>
+                <a href="#Contact" className="box-bar">
+                  CONTACT
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    </>
+  )
+}
