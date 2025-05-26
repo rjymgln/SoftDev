59ca03c7e1d7fd90f281b61fd29cc4a0e559fb98
Pldkoit0493 <geraldmariscotes@gmail.com>
2025-05-26 22:48:08 +0800

    styling changes

--- Frontend/src/components/Main Sections/MySwiper.js
+++ Frontend/src/components/Main Sections/MySwiper.js
@@ -8,24 +8,26 @@ function MySwiper() {
       <div className="bestproduct_title">
         <h1>Check Our Best Selling Coffee</h1>
       </div>
-      <Swiper
-        className="swiper-wrapper"
-        spaceBetween={150}
-        slidesPerView={1}
-        autoplay={{ delay: 5000 }}
-        speed={1000} // Adjusted speed for slower sliding animation
-      >
-        <SwiperSlide className="swiper-slide">
-          <img src={require('../../Assets/frappe1.png')} alt="" />
-        </SwiperSlide>
-        <SwiperSlide className="swiper-slide">
-          <img src={require('../../Assets/coffee1.png')} alt="" />
-        </SwiperSlide>
-        <SwiperSlide className="swiper-slide">
-          <img src={require('../../Assets/frappe2.png')} alt="" />
-        </SwiperSlide>
-      </Swiper>
-      <div className="autoplay-progress"></div>
+			<div className='swipercontainer'>
+				<Swiper
+					className="swiper-wrapper"
+					spaceBetween={150}
+					slidesPerView={1}
+					autoplay={{ delay: 5000 }}
+					speed={1000} // Adjusted speed for slower sliding animation
+				>
+					<SwiperSlide className="swiper-slide">
+						<img src={require('../../Assets/frappe1.png')} alt="" />
+					</SwiperSlide>
+					<SwiperSlide className="swiper-slide">
+						<img src={require('../../Assets/coffee1.png')} alt="" />
+					</SwiperSlide>
+					<SwiperSlide className="swiper-slide">
+						<img src={require('../../Assets/frappe2.png')} alt="" />
+					</SwiperSlide>
+				</Swiper>		
+				<div className="autoplay-progress"></div>
+			</div>
     </div>
   )
 }
